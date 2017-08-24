3a27616e2491363c361f9ae7ffa84f6dc441a383
Matt Juran <thepciet@gmail.com>
2017-08-24 15:19:25 -0500

    Added initial rook.

--- graphics/renderpieces.sh
+++ graphics/renderpieces.sh
@@ -2,3 +2,5 @@
 
 ./povrender.sh wpawn.pov ../web/img/wpawn.png
 ./povrender.sh bpawn.pov ../web/img/bpawn.png
+./povrender.sh wrook.pov ../web/img/wrook.png
+./povrender.sh brook.pov ../web/img/brook.png
---
+++ graphics/wrook.pov
@@ -0,0 +1,10 @@
+#version 3.7;
+
+#include "camera.inc"
+#include "finishes.inc"
+#include "rook.inc"
+
+object {
+    Rook
+    texture { WhiteT }
+}
