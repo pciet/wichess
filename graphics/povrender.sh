eb77feb2963dec97334bd5a628f4ea8300db8573
Matt Juran <thepciet@gmail.com>
2017-08-24 14:50:06 -0500

    Added initial render scripts and configuration, and images, for pawns.

---
+++ graphics/povrender.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Assumes povray 3.7 is installed.
+
+# Input file is first argument, output is second. Renders at 256x256 to a png.
+povray +I$1 +H256 +W256 +FN +O$2 
---
+++ graphics/renderpieces.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./povrender.sh wpawn.pov ../web/img/wpawn.png
+./povrender.sh bpawn.pov ../web/img/bpawn.png
