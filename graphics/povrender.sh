71359d8917acb5bf129c4f601d52754a6975abcc
Matt Juran <thepciet@gmail.com>
2017-09-02 11:29:27 -0500

    Generated piece images for a larger size and clearer finish.

--- graphics/povrender.sh
+++ graphics/povrender.sh
@@ -3,4 +3,4 @@
 # Assumes povray 3.7 is installed.
 
 # Input file is first argument, output is second. Renders at 256x256 to a png.
-povray +I$1 +H256 +W256 +FN +O$2 
+povray +I$1 +H256 +W256 Quality=11 +FN +O$2 
